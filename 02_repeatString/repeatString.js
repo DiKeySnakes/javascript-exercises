03f4aaa8ae97a1c3a294ab90e6fa4843c3e10376
DiKeySnakes <DiKeySnakes@gmail.com>
2022-10-06 23:27:12 +0300

    Modify repeatString.js and repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let result = '';
+  if (string === '') return '';
+  if (num === 0) return '';
+  if (num < 0) return 'ERROR';
+  for (let i = 0; i < num; i++) {
+    result = result + string;
+  }
+  return result;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+// console.log(repeatString('hey', -1));
