34e7b43cc3b2f3c0f3b10ebb87dec4f01b258e79
DiKeySnakes <DiKeySnakes@gmail.com>
2022-10-07 16:46:44 +0300

    Modify removeFromArray.js and removeFromArray.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...theArgs) {
+  let result;
+  let filtered = array;
+  let args = Array.from(theArgs);
+  for (let arg of args) {
+    result = filtered.filter((elem) => elem !== arg);
+    filtered = result;
+  }
+  console.log(args);
+  return result;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
