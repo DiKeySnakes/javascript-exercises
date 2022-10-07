43ec508d462a66d94e44f9be684cdbc17e0934f7
DiKeySnakes <DiKeySnakes@gmail.com>
2022-10-07 14:16:32 +0300

    Modify reverseString.js and reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let result = '';
+  if (string === '') return '';
+  result = string.split('').reverse().join('');
+  return result;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+console.log(reverseString('Hello World!'));
