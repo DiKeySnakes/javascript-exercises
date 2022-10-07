5b0cd64629d0e91dceaf060ad25f6b79110ebe6d
DiKeySnakes <DiKeySnakes@gmail.com>
2022-10-07 20:58:13 +0300

    Modify tempConversion.js and tempConversion.spec.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function (fTemp) {
+  let result = (5 / 9) * (fTemp - 32);
+  return parseFloat(result.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (cTemp) {
+  let result = (cTemp * 9) / 5 + 32;
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
+
+console.log(ftoc(100));
+console.log(ctof(73.2));
